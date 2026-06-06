b97331e680a4e1590f9670afdd0d710bbafb6182
AleksanderMaznio <am137137@stud.ur.edu.pl>
2026-06-06 15:04:30 +0200

    wowow

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@ Route::get('/', function () {
 Route::get('/import', function () {
     return view('import');
 });
+use App\Http\Controllers\ImportController;
+
+Route::get('/import', function () {
+    return view('import');
+});
+
+// To odbierze plik po kliknięciu przycisku
+Route::post('/import', [ImportController::class, 'store']);
